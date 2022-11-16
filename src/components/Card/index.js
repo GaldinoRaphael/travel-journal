a755313d76ceea800fe6d7c65eda0e6d03376647
raphaelgadino <raphael.costa@db1.com.br>
2022-11-16 14:05:25 -0300

    fix: layout and import

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './style.css';
 
-import Photo from '../../imgs/mount-fuji.svg';
 import LocationPin from '../../imgs/location-pin.svg';
 
 export default function Card({item}){
@@ -16,7 +15,7 @@ export default function Card({item}){
                         <a className="card-informations--location-link light-grey" href={item.googleMapsUrl} target="_blank">View on Google Maps</a>
                     </p>
                     <h1 className="card-informations--title bold grey">{item.title}</h1>
-                    <p className="card-informations--date bold grey">{item.startDate}</p>
+                    <p className="card-informations--date bold grey">{`${item.startDate} - ${item.endDate}`}</p>
                     <p className="card-informations--description grey">{item.description}</p>
                 </section>
                 <hr></hr>
